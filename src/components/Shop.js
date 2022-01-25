dda5f481c2dff9a8e506b8378cd70200739bdf0d
jonahBerlyne <jonshaber@gmail.com>
2022-01-25 07:05:38 -0500

    706am 1/25/22 added fetching

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,18 +1,46 @@
+import React, { useState, useEffect } from 'react';
 import NavBar from "./NavBar";
 import store from "../store";
 
 export default function Shop() {
 
- function getState() {
+ useEffect(() => {
+  fetchItems();
+ }, []);
 
+ // const [refresh, setRefresh] = useState(false);
+
+ // useEffect(() => {
+ //   setRefresh(!refresh);
+ // }, [items]);
+
+ function getState() {
   const state = store.getState();
   console.log("state is " + state + " and the shop length is " + state.length);
  }
+
+ const [items, setItems] = useState([]);
+
+ const fetchItems = async () => {
+  const data = await fetch("https://www.easports.com/fifa/ultimate-team/api/fut/item", {method: 'get', mode: 'no-cors'});
+  console.log(data);
+  const dataItems = await data.json();
+  setItems(dataItems);
+ };
+
   return (
     <div className="App">
       <NavBar/>
       <h1>Shop Page</h1>
       <button onClick={getState}>Get State</button>
+      <h2>Items:</h2>
+      {/* {items.map(item => {
+       return (
+        <div key={item.id}>
+         <h3>{item.title}</h3>
+        </div>
+       )}
+      )} */}
     </div>
   );
 }
