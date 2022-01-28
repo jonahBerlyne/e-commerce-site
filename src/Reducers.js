f1683e8dbd66b91698db0e34e7391af270cbfc3a
jonahBerlyne <jonshaber@gmail.com>
2022-01-28 12:13:01 -0500

    1213pm 1/28/22 fixed decrease reducer

--- src/Reducers.js
+++ src/Reducers.js
@@ -12,7 +12,7 @@ export default function reducer(state = [], action) {
 
   case actions.ITEM_DECREASED:
 
-    return state.map(item => item.id === action.payload.id ? {...item, quantity: item.quantity - 1, price: parseFloat((action.payload.price * (item.quantity - 1)).toFixed(2))} : item);
+    return state.map(item => item.id === action.payload.id ? {...item, quantity: item.quantity - 1, price: parseFloat((action.payload.price - (action.payload.price * (1 / item.quantity))).toFixed(2))} : item);
 
   case actions.ITEM_REMOVED:
 
