b15f4c900484c31d5a78d9f1717955d02167682b
jonahBerlyne <jonshaber@gmail.com>
2022-02-01 14:56:51 -0500

    257pm 2/1/22 added firebase config

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,11 @@
 import NavBar from "./NavBar";
+import firebase from "../firebase";
 
 export default function Home() {
+
+  const ref = firebase.firestore().collection("demo");
+  console.log(ref);
+
   return (
     <div className="App">
       <NavBar/>
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/compat/app";
+import "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7Nq0NhcXzQHsZvF5Y-3WcYSmqjSiYzSo",
+  authDomain: "e-commerce-site-6836a.firebaseapp.com",
+  projectId: "e-commerce-site-6836a",
+  storageBucket: "e-commerce-site-6836a.appspot.com",
+  messagingSenderId: "926072447686",
+  appId: "1:926072447686:web:61ddc085a8188bc1662dc7"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
