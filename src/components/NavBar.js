06f36903c35864c486ac1b35aa27667f7f103935
jonahBerlyne <jonshaber@gmail.com>
2022-02-08 19:10:41 -0500

    installed bootstrap and added navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,33 @@
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 export default function NavBar() {
 
- const navStyle = {
-  backgroundColor: "DodgerBlue",
-  padding: "30px",
-  margin: "-10px",
-  fontSize: "25px",
-  fontFamily: "helvetica",
-  textAlign: "center",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
- };
-
- const linkStyle = {
-  textDecoration: "none",
-  cursor: "pointer",
-  color: "darkRed",
-  backgroundColor: "green",
-  padding: "10px",
- };
-
  return (
-  <nav style={navStyle}>
-   <div style={{paddingRight: "100px"}}>
-    Welcome!
+    <div className='header'>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <h1 className="navbar-brand">Welcome!</h1>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span>
+                <FaBars size={25} color="gray"/>
+              </span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/shop">Shop</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/cart">Cart</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+    </nav>
    </div>
-   <div style={{display: "flex", gap: "20px"}}>
-    <Link to="/" style={linkStyle}>Home</Link>
-    <Link to="/shop" style={linkStyle}>Shop</Link>
-    <Link to="/cart" style={linkStyle}>Cart</Link>
-   </div>
-  </nav>
- );
+  );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import store from "./Redux/Store";
 import { Provider } from "react-redux";
-// import FirebaseProvider from './firebase/compat/firebase';
 
 ReactDOM.render(
-  // <FirebaseProvider></FirebaseProvider>
     <Provider store={store}>
       <App />
     </Provider>,
