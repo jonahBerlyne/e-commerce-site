ba29a17c284389da020055ef58b11de69c207c03
jonahBerlyne <jonshaber@gmail.com>
2022-04-16 18:38:20 -0400

    added auth state tracker

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Components/Home";
 import Shop from "./Components/Shop";
@@ -9,23 +10,51 @@ import Login from "./Components/Login";
 import NavBar from "./Components/NavBar";
 import Orders from "./Components/Orders";
 import Audio from "./Components/Audio";
+import fireDB from "./firebaseConfig";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default function App() {
+
+  const AuthNav = () => {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/register" exact element={<AuthRoutes><Register/></AuthRoutes>}/>
+          <Route path="/login" exact element={<AuthRoutes><Login/></AuthRoutes>}/>
+        </Routes>
+      </Router>
+    );
+  }
+
+  const AppNav = () => {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<ProtectedRoutes><Home/></ProtectedRoutes>}/>
+          <Route path="/shop" exact element={<ProtectedRoutes><Shop/></ProtectedRoutes>}/>
+          <Route path="/shop/:id" exact element={<ProtectedRoutes><ItemDetail/></ProtectedRoutes>}/>
+          <Route path="/cart" exact element={<ProtectedRoutes><Cart/></ProtectedRoutes>}/>
+          <Route path="/orders" exact element={<ProtectedRoutes><Orders/></ProtectedRoutes>}/>
+          <Route path="/checkout" exact element={<CheckoutRoute><CheckoutPage/></CheckoutRoute>}/>
+        </Routes>
+      </Router>
+    );
+  }
+
+  const auth = getAuth();
+
+  const [user, setUser] = useState(null);
+
+  onAuthStateChanged(auth, _user => {
+    _user ? setUser(_user) : setUser(null);
+  });
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<ProtectedRoutes><Home/></ProtectedRoutes>}/>
-        <Route path="/shop" exact element={<ProtectedRoutes><Shop/></ProtectedRoutes>}/>
-        <Route path="/shop/:id" exact element={<ProtectedRoutes><ItemDetail/></ProtectedRoutes>}/>
-        <Route path="/cart" exact element={<ProtectedRoutes><Cart/></ProtectedRoutes>}/>
-        <Route path="/orders" exact element={<ProtectedRoutes><Orders/></ProtectedRoutes>}/>
-        <Route path="/checkout" exact element={<CheckoutRoute><CheckoutPage/></CheckoutRoute>}/>
-        <Route path="/register" exact element={<AuthRoutes><Register/></AuthRoutes>}/>
-        <Route path="/login" exact element={<AuthRoutes><Login/></AuthRoutes>}/>
-      </Routes>
-    </Router>
+    <div>
+      {user ? <AppNav /> : <AuthNav />}
+    </div>
   );
-}
+} 
 
 const ProtectedRoutes = ({children}) => {
   if (localStorage.getItem("currentUser")) {
