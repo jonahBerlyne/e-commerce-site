e0f0d9c4832d01fdbd90062ace0328d8108199d9
jonahBerlyne <jonshaber@gmail.com>
2022-04-20 22:43:20 -0400

    created privateroute file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Components/Home";
 import Shop from "./Components/Shop";
@@ -9,52 +8,24 @@ import Register from "./Components/Register";
 import Login from "./Components/Login";
 import NavBar from "./Components/NavBar";
 import Orders from "./Components/Orders";
-import Audio from "./Components/Audio";
-import fireDB from "./firebaseConfig";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 export default function App() {
-
-  const AuthNav = () => {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/register" exact element={<AuthRoutes><Register/></AuthRoutes>}/>
-          <Route path="/login" exact element={<AuthRoutes><Login/></AuthRoutes>}/>
-        </Routes>
-      </Router>
-    );
-  }
-
-  const AppNav = () => {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<ProtectedRoutes><Home/></ProtectedRoutes>}/>
-          <Route path="/shop" exact element={<ProtectedRoutes><Shop/></ProtectedRoutes>}/>
-          <Route path="/shop/:id" exact element={<ProtectedRoutes><ItemDetail/></ProtectedRoutes>}/>
-          <Route path="/cart" exact element={<ProtectedRoutes><Cart/></ProtectedRoutes>}/>
-          <Route path="/orders" exact element={<ProtectedRoutes><Orders/></ProtectedRoutes>}/>
-          <Route path="/checkout" exact element={<CheckoutRoute><CheckoutPage/></CheckoutRoute>}/>
-        </Routes>
-      </Router>
-    );
-  }
-
-  const auth = getAuth();
-
-  const [user, setUser] = useState(null);
-
-  onAuthStateChanged(auth, _user => {
-    _user ? setUser(_user) : setUser(null);
-  });
-
   return (
-    <div>
-      {user ? <AppNav /> : <AuthNav />}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<ProtectedRoutes><Home/></ProtectedRoutes>}/>
+        <Route path="/shop" exact element={<ProtectedRoutes><Shop/></ProtectedRoutes>}/>
+        <Route path="/shop/:id" exact element={<ProtectedRoutes><ItemDetail/></ProtectedRoutes>}/>
+        <Route path="/cart" exact element={<ProtectedRoutes><Cart/></ProtectedRoutes>}/>
+        <Route path="/orders" exact element={<ProtectedRoutes><Orders/></ProtectedRoutes>}/>
+        <Route path="/checkout" exact element={<CheckoutRoute><CheckoutPage/></CheckoutRoute>}/>
+        <Route path="/register" exact element={<AuthRoutes><Register/></AuthRoutes>}/>
+        <Route path="/login" exact element={<AuthRoutes><Login/></AuthRoutes>}/>
+      </Routes>
+    </Router>
   );
-} 
+}
 
 const ProtectedRoutes = ({children}) => {
   if (localStorage.getItem("currentUser")) {
@@ -63,7 +34,6 @@ const ProtectedRoutes = ({children}) => {
       <div>
         <NavBar/>
         {children}
-        <footer><Audio/></footer>
       </div>
     );
   } else {
@@ -91,4 +61,3 @@ const AuthRoutes = ({children}) => {
     return <Navigate to="/"/>;
   }
 }
-
---
+++ src/PrivateRoute.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { Route, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
+
+export default function PrivateRoute (props) {
+ const [pending, setPending] = useState(true);
+ const [currentUser, setCurrentUser] = useState(null);
+ const auth = getAuth();
+
+ useEffect(() => {
+  const unsub = onAuthStateChanged(
+   auth,
+   user => {
+    user ? setCurrentUser(user) : setCurrentUser(null);
+    setPending(false);
+   },
+   err => {
+    alert(`Error: ${err}`);
+    setPending(false);
+   }
+  );
+
+  return unsub;
+ }, []);
+
+ if (pending) return null;
+
+ return currentUser
+  ? <Route {...props} />
+  : <Navigate to="/login" />;
+}
