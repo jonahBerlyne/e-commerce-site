942280f5f9bcf6956ffbb9cc94a9924c04dde342
jonahBerlyne <jonshaber@gmail.com>
2022-04-25 01:49:22 -0400

    reconfigured index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from "./Redux/Store";
 import { Provider } from "react-redux";
 import "./index.css";
 
-ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
