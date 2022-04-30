c82fe2bbb1bfde3aa8cef30e6624407ec392f5a2
jonahBerlyne <jonshaber@gmail.com>
2022-04-29 20:32:56 -0400

    added types to action types

--- src/Redux/ActionTypes.tsx
+++ src/Redux/ActionTypes.tsx
@@ -1,5 +1,5 @@
-export const ITEM_ADDED = "itemAdded";
-export const ITEM_DECREASED = "itemDecreased";
-export const ITEM_INCREASED = "itemIncreased";
-export const ITEM_REMOVED = "itemRemoved";
-export const ITEM_SET =  "itemSet";
+export const ITEM_ADDED: string = "itemAdded";
+export const ITEM_DECREASED: string = "itemDecreased";
+export const ITEM_INCREASED: string = "itemIncreased";
+export const ITEM_REMOVED: string = "itemRemoved";
+export const ITEM_SET: string =  "itemSet";
