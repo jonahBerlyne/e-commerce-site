453180735feab7687e3fa80b01ad69b7a1ac28fe
jonahBerlyne <jonshaber@gmail.com>
2022-04-30 22:10:00 -0400

    moved cart retrieval fn from home page to app route

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -6,30 +6,6 @@ import { useEffect } from "react";
 import { useDispatch } from 'react-redux';
 
 export default function Home() {
-  const { user } = JSON.parse(localStorage.getItem("currentUser") || "{}");
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    retrieveCartItems(user.uid);
-  }, []);
-
-  const retrieveCartItems = async (id: any): Promise<any> => {
-    try {
-      const q = query(collection(fireDB, "users", id, "items"));
-      const querySnapshot = await getDocs(q);
-      let itemsArr: any[] = [];
-      querySnapshot.forEach(doc => {
-        itemsArr.push(doc.data());
-      });
-      itemsArr.forEach(item => {
-        dispatch(itemSet(item.id, item.title, item.image, item.price, item.quantity));
-      });
-      const cart = store.getState();
-      localStorage.setItem("cart", JSON.stringify(cart));
-    } catch (err) {
-      alert(`Error: ${err}`);
-    }
-  }
 
   return (
     <div className="App">
--- src/Routes/AppRoute.tsx
+++ src/Routes/AppRoute.tsx
@@ -1,20 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Navigate } from 'react-router-dom';
-import { onAuthStateChanged } from 'firebase/auth';
-import { getAuth } from 'firebase/auth';
+import { onAuthStateChanged, getAuth } from 'firebase/auth';
 import NavBar from '../Components/NavBar';
 import Audio from '../Components/Audio';
+import fireDB from '../firebaseConfig';
+import { collection, query, getDocs } from "firebase/firestore";
+import store from '../Redux/Store';
+import { itemSet } from '../Redux/Actions';
+import { useDispatch } from 'react-redux';
 
 export default function AppRoute ({children}: {children: any}) {
- const [pending, setPending] = useState<boolean>(true);
- const [currentUser, setCurrentUser] = useState<any>(null);
- const auth = getAuth();
+  const [pending, setPending] = useState<boolean>(true);
+  const [currentUser, setCurrentUser] = useState<any>(null);
+  const auth = getAuth();
+  const dispatch = useDispatch();
+
+  const retrieveCartItems = async (id: any): Promise<any> => {
+    try {
+      const q = query(collection(fireDB, "users", id, "items"));
+      const querySnapshot = await getDocs(q);
+      let itemsArr: any[] = [];
+      querySnapshot.forEach(doc => {
+        itemsArr.push(doc.data());
+      });
+      itemsArr.forEach(item => {
+        dispatch(itemSet(item.id, item.title, item.image, item.price, item.quantity));
+      });
+      const cart = store.getState();
+      localStorage.setItem("cart", JSON.stringify(cart));
+    } catch (err) {
+      alert(`Error: ${err}`);
+    }
+  }
 
  useEffect(() => {
   const unsub = onAuthStateChanged(
    auth,
    user => {
-    user ? setCurrentUser(user) : setCurrentUser(null);
+    if (user) {
+      setCurrentUser(user);
+      retrieveCartItems(user.uid);
+    } else {
+      setCurrentUser(null);
+    }
     setPending(false);
    },
    err => {
