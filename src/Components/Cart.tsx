be290b7cc34dd9ce5af18a0d6db96e52c0d0f3f4
jonahBerlyne <jonshaber@gmail.com>
2022-05-21 18:24:21 -0400

    improved cart sidebar

--- src/Components/Cart.tsx
+++ src/Components/Cart.tsx
@@ -7,7 +7,7 @@ import { Delete } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
 import { useAppSelector, useAppDispatch } from '../Redux/Hooks';
 import { closeCart, selectCart } from '../Redux/Slices/cartSlice';
-import { Modal } from 'react-bootstrap';
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Cart() {
 
@@ -83,109 +83,95 @@ export default function Cart() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
+  const backdrop = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  };
+
+  const cartDisplay = {
+    hidden: { x: "100%" },
+    visible: { x: "0" },
+  };
+
   return (
-     <Modal
-        show={cart}
-        onHide={() => dispatch(closeCart())}
-        dialogClassName="modal-90w"
-        className="modal right fade"
-        animation
-      >
-        <Modal.Header closeButton className="modal-header">
-          <Modal.Title className='modal-title'>
-            Cart
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="body">
-             {items.length === 0 && <h4>Your cart is empty.</h4>}
-             {items.map(item => {
-              return (
-                <div key={item.id}>
-                  <h3>{item.title}</h3>
-                  <img src={item.image} alt={item.title} height="100px" width="100px"/>
-                  <br/>
-                  <br/>
-                  <div style={{display: "flex", gap: "5px"}}>
-                    <button 
-                      className='btn btn-outline-secondary' 
-                      onClick={() => handleItem("decreaseItem", item.id)} 
-                      disabled={item.quantity === 1}
-                    >-
-                    </button>
-                    <h4>{item.quantity}</h4>
-                    <button 
-                      className='btn btn-outline-secondary' 
-                      onClick={() => handleItem("increaseItem", item.id)}
-                    >+
-                    </button>
-                  </div>
-                  <h4>Price: ${parseFloat(item.total).toFixed(2)}</h4>
-                  <IconButton onClick={() => handleItem("removeItem", item.id)}>
-                    <Delete sx={{ color: "red" }} />
-                  </IconButton>
+    <AnimatePresence>
+      {cart && (
+        <>
+          <motion.div 
+            variants={backdrop}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            onClick={() => dispatch(closeCart())}
+            transition={{ ease: "easeOut", duration: 0.5 }}
+            className="backdrop"
+          />
+          <motion.div
+            variants={cartDisplay}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            transition={{ ease: "easeOut", duration: 0.5 }}
+            className="cart"
+          >
+            <div className="container">
+              <button onClick={() => dispatch(closeCart())} className='close'>
+                <svg
+                  className="svgClose"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z" />
+                </svg>
+              </button>
+              <div className="cart-content-container">
+                <h1 className="title">Cart</h1>
+                <div className="cart-list">
+                  {items.length === 0 && <h4 className='empty-cart'>Your cart is empty.</h4>}
+                  {items.map(item => {
+                    return (
+                      <div key={item.id}>
+                        <h3>{item.title}</h3>
+                        <img src={item.image} alt={item.title} height="100px" width="100px"/>
+                        <br/>
+                        <br/>
+                        <div style={{display: "flex", gap: "5px"}}>
+                          <button 
+                            className='btn btn-outline-secondary' 
+                            onClick={() => handleItem("decreaseItem", item.id)} 
+                            disabled={item.quantity === 1}
+                          >-
+                          </button>
+                          <h4>{item.quantity}</h4>
+                          <button 
+                            className='btn btn-outline-secondary' 
+                            onClick={() => handleItem("increaseItem", item.id)}
+                          >+
+                          </button>
+                        </div>
+                        <h4>Price: ${parseFloat(item.total).toFixed(2)}</h4>
+                        <IconButton onClick={() => handleItem("removeItem", item.id)}>
+                          <Delete sx={{ color: "red" }} />
+                        </IconButton>
+                      </div>
+                    );
+                  })}
+                  {totalPrice > 0 && 
+                    <div>
+                      <h2 className='total-price'>Total Price: ${totalPrice.toFixed(2)}</h2>
+                      <button onClick={() => navigate("/checkout")}>Checkout</button>
+                    </div>
+                  }
                 </div>
-              );
-            })}
-            {totalPrice > 0 && 
-              <div>
-                <h2>Total Price: ${totalPrice.toFixed(2)}</h2>
-                <button onClick={() => navigate("/checkout")}>Checkout</button>
               </div>
-            }
-        </Modal.Body>
-      </Modal>
+
+            </div>
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
   );
-
-  // return (
-  //   <div className="modal right fade">
-  //     <div className="modal-dialog">
-  //       <div className="modal-content">
-
-  //         <div className="modal-header">
-  //           <button type='button' className='close' onClick={() => dispatch(closeCart())}><span>&times;</span></button>
-  //           <h2 className='modal-title'>Cart</h2>
-  //         </div>
-
-  //         <div className="modal-body">
-  //           {items.length === 0 && <h4>Your cart is empty.</h4>}
-  //           {items.map(item => {
-  //             return (
-  //               <div key={item.id}>
-  //                 <h3>{item.title}</h3>
-  //                 <img src={item.image} alt={item.title} height="100px" width="100px"/>
-  //                 <br/>
-  //                 <br/>
-  //                 <div style={{display: "flex", gap: "5px"}}>
-  //                   <button 
-  //                     className='btn btn-outline-secondary' 
-  //                     onClick={() => handleItem("decreaseItem", item.id)} 
-  //                     disabled={item.quantity === 1}
-  //                   >-
-  //                   </button>
-  //                   <h4>{item.quantity}</h4>
-  //                   <button 
-  //                     className='btn btn-outline-secondary' 
-  //                     onClick={() => handleItem("increaseItem", item.id)}
-  //                   >+
-  //                   </button>
-  //                 </div>
-  //                 <h4>Price: ${parseFloat(item.total).toFixed(2)}</h4>
-  //                 <IconButton onClick={() => handleItem("removeItem", item.id)}>
-  //                   <Delete sx={{ color: "red" }} />
-  //                 </IconButton>
-  //               </div>
-  //             );
-  //           })}
-  //           {totalPrice > 0 && 
-  //             <div>
-  //               <h2>Total Price: ${totalPrice.toFixed(2)}</h2>
-  //               <button onClick={() => navigate("/checkout")}>Checkout</button>
-  //             </div>
-  //           }
-  //         </div>
-
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 }
